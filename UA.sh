55503956580b693d432d68572a0b3d0fd9131273
rickeymandraque <rickeymandraque@users.noreply.github.com>
2024-01-23 12:57:09 +0100

    Create UA.sh

---
+++ UA.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+CoffeeLover="$(curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 CoffeeBot/1.0")"
+LostDev="$(curl -A "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko Firefox/99.0 (LOST-IE)")"
+CaptainObvious="$(curl -A "TotallyNotACurlScript/1.0 (definitely not pretending to be Chrome/99.0.9999.99)")"
+CryptoNinja="$(curl -A "BitcoinMiner/9000.0 (X11; Linux x86_64) Brave/42.0.0.0 Safari/1337.7")"
+NotAPinguin="$(curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/99.0.0.0 (Not Linux Penguin)")"
+TempleOS="$(curl -A "TempleOSExplorer/1.0 (TempleBot/16.0; HolyCScript) DivineEngine/20231220")" # The OS of God !
+DavidBowie="$(curl -A "ZiggyStardust/2.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko Firefox/99.0 (SingingSpider)")"
+ImNotABot="$(curl -A "Mozilla/5.0 (Imnotabot Windows OSX 10.0; Linux_64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/99.0.0.0 (I promise)")"
+PS2W11="$(curl -A "Mozilla/5.0 (PlayStation; PlayStation 2/8.00; Windows NT 10.0) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Safari/605.1.15")"
